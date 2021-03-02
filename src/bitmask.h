3b6ffc33aa0be08d1008d6c048ee89f1f0931235
stepin104426 <69068157+stepin104426@users.noreply.github.com>
2021-03-02 14:37:20 +0530

    up

--- src/bitmask.h
+++
@@ -1,7 +0,0 @@
-#ifndef __BITMASK_H
-#define __BITMASK_H
-#include<stdlib.h>
-int set(int a, int b);
-int clear(int a, int b);
-int flip(int a, int b);
-#endif
--- src/myutils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MYUTILS_H
-#define __MYUTILS_H
-int factorial(int x);
-int isPrime(int x);
-int isPalindrome(int x);
-int vsum(int x, ...);
-
-#endif
