4b669dc00adf3c65ffd93856a8329eb58843c3c7
99003554 <79654940+99003554@users.noreply.github.com>
2021-03-02 08:10:14 +0530

    Update mystring.h

--- mystring.h
+++ mystring.h
@@ -1,21 +1,10 @@
 #ifndef __MYSTRING_H
 #define __MYSTRING_H
-// mystrlen, mystrcpy, mystrcat, mystrcmp  
+#include<string.h>
 
-char *substring(int i,int j,char *ch)
-{
-    int n,k=0;
-    char *ch1;
-    ch1=(char*)malloc((j-i+1)*1);
-    n=j-i+1;
-
-    while(k<n)
-    {
-        ch1[k]=ch[i];
-        i++;k++;
-    }
-
-    return (char *)ch1;
-}
+size_t mystrlen(const char *str);
+char* mystrcpy(char* str1, const char* str2);
+char* mystrcat(char *str1, const char *str2);
+int mystrcmp(const char *str1, const char *str2 );
 
 #endif
