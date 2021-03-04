92cefe65c98a5b972fd328e2631a40b289575ff2
99003554 <79654940+99003554@users.noreply.github.com>
2021-03-04 06:35:08 +0530

    Update th1.c

--- 3-3-21/act/thread/th1.c
+++ 3-3-21/act/thread/th1.c
@@ -9,7 +9,6 @@ int a[max] = { 1, 5, 7, 10, 12, 14, 15, 18, 20,
 // Array to store max of threads 
 int max[Th_max] = { 0 }; 
 int no = 0; 
-  
 // Function to find maximum 
 void maximum(void* arg) 
 { 
@@ -19,36 +18,28 @@ void maximum(void* arg)
     for (i = num * (max / 4); i < (num + 1) * (max / 4); i++) { 
         if (a[i] > maxs) 
             maxs = a[i]; 
-    } 
-  
+    }   
     max[num] = maxs; 
 } 
-  
-// Driver code 
+// Main function
 int main() 
 { 
     int maxs = 0; 
     int i; 
     pthread_t threads[Th_max]; 
-  
-    // creating 4 threads 
+     // creating 4 threads 
     for (i = 0; i < Th_max; i++) 
-        pthread_create(&threads[i], NULL, 
-                       maximum, (void*)NULL); 
-  
-    // joining 4 threads i.e. waiting for 
+        pthread_create(&threads[i], NULL, maximum, (void*)NULL); 
+      // joining threads 
     // all 4 threads to complete 
     for (i = 0; i < Th_max; i++) 
         pthread_join(threads[i], NULL); 
-  
-    // Finding max element in an array 
+      // Finding max element in an array 
     // by individual threads 
     for (i = 0; i < Th_max; i++) { 
         if (max_num[i] > maxs) 
             maxs = max_num[i]; 
-    } 
-  
-    printf("Maximum Element is : %d", maxs); 
-  
+    }   
+    printf("Max: %d", maxs); 
     return 0; 
 } 
