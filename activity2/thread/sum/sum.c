d471eee41c49e999d204e9ae8359d113bd3917a5
99003554 <79654940+99003554@users.noreply.github.com>
2021-03-06 01:13:51 +0530

    Update sum.c

--- activity2/thread/sum/sum.c
+++ activity2/thread/sum/sum.c
@@ -1,28 +1,39 @@
 #include <stdio.h> 
 #include <pthread.h> 
  // size of array 
-#define MAX 16
+//#define MAX 1
 // maximum number of threads 
 #define MAX_THREAD 5 
-int a[] = { 0, 15, 27, 10, 52, 34, 22, 14, 13, 42, 5, 25, 30, 4, 120, 240 }; 
-int sum[5] = { 0 }; 
+int a[1000] = {  }; 
+int sum[5] = {}; 
 int part = 0; 
 //Thread for summ  
 void* sum_array(void* arg) 
 { 
     // Each thread computes sum 
-    int thread_part = part++; 
-     for (int i = thread_part * (MAX / 4); i < (thread_part + 1) * (MAX / 4); i++) 
-        sum[thread_part] += a[i]; 
+	  int i, ps, temp, thread_num = (__intptr_t)arg;
+  for (i = 0; i < 100; i++)
+    /* Adding 16 array values */
+    sum[i] += a[ thread_num * 100 + i]; 
+    //int thread_part = part++; 
+     //for (int i = thread_part * (MAX / 4); i < (thread_part + 1) * (MAX / 4); i++) 
+       // sum[thread_part] += a[i]; 
 } 
   
-// Driver Code 
-int main() 
-{ 
-      pthread_t threads[MAX_THREAD]; 
+// Code 
+int sum() 
+{ 	
+	int i;
+	//pthread_t ptarr[n];
+	srand(time(0));
+    pthread_t threads[MAX_THREAD]; 
+	for(i=0; i<1000; i++) {
+    /* Assigning random value to Array */
+		a[i] = random()%10;
+	             }
       // Creating 5 threads 
     for (int i = 0; i < MAX_THREAD; i++) 
-        pthread_create(&threads[i], NULL, sum_array, (void*)NULL); 
+        pthread_create(&threads[i], NULL, sum_array, (void*)(__intptr_t)i); 
       // joining 5 threads  
     for (int i = 0; i < MAX_THREAD; i++) 
     {
@@ -32,7 +43,12 @@ int main()
     int total_sum = 0; 
     for (int i = 0; i < MAX_THREAD; i++) 
      {   total_sum += sum[i]; }
-      printf("Sum is \n" ,total_sum); 
-     
-      return 0; 
+      //printf("Sum is \n" ,total_sum); 
+       return total_sum; 
 } 
+
+int main()
+{
+  printf("Sum of 1000 random numbers : %d\n", sum()); /* Print the sum */
+  return 0;
+}
