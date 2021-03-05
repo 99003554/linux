d7fb0bd07ac9cc50a56dec6e2297e8d3d910952b
99003554 <ragulkrishna.periaswamy@ltts.com>
2021-03-05 17:29:41 +0530

    latest

--- activity2/thread/sum/sum.c
+++ activity2/thread/sum/sum.c
@@ -1,12 +1,9 @@
 #include <stdio.h> 
 #include <pthread.h> 
-  
-// size of array 
-#define MAX 12
-  
+ // size of array 
+#define MAX 16
 // maximum number of threads 
 #define MAX_THREAD 5 
-  
 int a[] = { 0, 15, 27, 10, 52, 34, 22, 14, 13, 42, 5, 25, 30, 4, 120, 240 }; 
 int sum[5] = { 0 }; 
 int part = 0; 
@@ -22,22 +19,20 @@ void* sum_array(void* arg)
 // Driver Code 
 int main() 
 { 
-  
-    pthread_t threads[MAX_THREAD]; 
-  
-    // Creating 5 threads 
+      pthread_t threads[MAX_THREAD]; 
+      // Creating 5 threads 
     for (int i = 0; i < MAX_THREAD; i++) 
         pthread_create(&threads[i], NULL, sum_array, (void*)NULL); 
       // joining 5 threads  
     for (int i = 0; i < MAX_THREAD; i++) 
+    {
         pthread_join(threads[i], NULL); 
-  
-    // adding sum of all 4 parts 
+    }
+      // adding sum of all 4 parts 
     int total_sum = 0; 
     for (int i = 0; i < MAX_THREAD; i++) 
-        total_sum += sum[i]; 
-  
-    cout << "sum is " << total_sum << endl; 
-  
-    return 0; 
+     {   total_sum += sum[i]; }
+      printf("Sum is \n" ,total_sum); 
+     
+      return 0; 
 } 
--- activity3/activity/semaphore.c
+++ activity3/activity/semaphore.c
@@ -1,7 +1,8 @@
 #include<semaphore.h>
-#include<pthread.h>
+//#include<pthread.h>
+#include "semaphore.h"
 //const int max=15;
-int m_arr[15];
+int m_arr[10];
 int m_top=-1;
 sem_t s1;
 sem_t s2;
@@ -11,7 +12,7 @@ void* push(void* pv)	//consumer
     int val=(int*)pv;
 	sem_wait(&s1);
   // pthread_mutex_lock(&m1);
-	if(m_top<=15){
+	if(m_top<=10){
         m_arr[++m_top]=val;
          printf("push val:%d\n",val);
     }
@@ -29,7 +30,7 @@ void* pop(void* pv)	//producer
 	if(m_top!=-1){
         //temp+=1;
         temp=m_arr[m_top--];
-        printf("VAlue:%d\n",temp);
+        printf("Value:%d\n",temp);
     }
    //pthread_mutex_unlock(&m1);
     sem_post(&s1);
